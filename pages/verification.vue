7b674e4c4fbec3484e7be24d189bb807924b42c3
Manuel Navarro Pérez <manuelnavarro1987@gmail.com>
2017-10-23 09:33:03 +0200

    Verification page and component

---
+++ pages/verification.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  verification
+</template>
+
+<script>
+import Verification from '~/components/Verification.vue'
+
+export default {
+  components: {
+    Verification
+  }
+}
+</script>
