903d639eb6520b51c981e461f302ca6bd710cc06
Manuel Navarro Perez <manuel.navarro@macpro-manuel-navarro.local>
2017-10-23 09:33:28 +0200

    ForgotPassword page and component

---
+++ components/ForgotPassword.vue
@@ -0,0 +1,69 @@
+<template lang="pug">
+v-layout(row justify-center align-center fill-height)
+  v-flex(xs12 sm8 md6)
+    v-card
+      v-card-title.headline Forgot the password
+      v-card-text
+        form(v-if="showPasswordForm" @keydown.enter.prevent="sendActivatePassword")
+          v-text-field(label="Code", v-model="code", required)
+          v-text-field(type="password" label="Password", hint="The length must be more than 8 characters", v-model="password" required)
+          v-layout(row justify-space-between)
+            small *indicates required field
+            v-btn(color="primary" :loading="loading"  @click.prevent="sendActivatePassword" :disabled="loading") Set password
+          v-alert(error dismissible transition="scale-transition" v-model="alert") {{error}}
+        form(v-else @keydown.enter.prevent="sendForgotPassword")
+          v-text-field(type="email" label="Email", v-model="email", required)
+          v-layout(row justify-space-between)
+            small *indicates required field
+            v-btn(color="primary" :loading="loading"  @click.prevent="sendForgotPassword" :disabled="loading") Forgot password
+          v-alert(error dismissible transition="scale-transition" v-model="alert") {{error}}
+          v-layout(row justify-space-between)
+            v-btn(color="primary" small flat :to="'/'") Have you an account?
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showPasswordForm: false,
+      email: '',
+      code: '',
+      password: '',
+      loading: false,
+      alert: false,
+      error: ''
+    }
+  },
+  methods: {
+    async sendForgotPassword() {
+      this.loading = true
+      try {
+        this.showPasswordForm = await this.$store.dispatch('auth/FORGOT_PASSWORD', { email: this.email.trim() })
+        this.alert = false
+      } catch (error) {
+        this.error = error.message
+        this.alert = true
+      }
+      this.loading = false
+    },
+    async sendActivatePassword() {
+      this.loading = true
+      try {
+        this.showPasswordForm = await this.$store.dispatch(
+          'auth/CONFIRM_PASSWORD',
+          {
+            email: this.email.trim(),
+            code: this.code.trim(),
+            password: this.password.trim()
+          }
+        )
+        this.$router.push({ name: 'index' })
+      } catch (error) {
+        this.error = error.message
+        this.alert = true
+      }
+      this.loading = false
+    }
+  }
+}
+</script>
