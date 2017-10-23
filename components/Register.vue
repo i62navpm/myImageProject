c99742a9cd6e3d7e9bad71be15c4b3f5a817b4a7
Manuel Navarro Perez <manuel.navarro@macpro-manuel-navarro.local>
2017-10-23 09:32:40 +0200

    Register page and component

---
+++ components/Register.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+v-layout(row justify-center align-center fill-height)
+  v-flex(xs12 sm8 md6)
+    v-card
+      v-card-title.headline Register
+      v-card-text
+        form(@keyup.enter="sendRegister")
+          v-text-field(type="email" label="Email", v-model="email", required)
+          v-text-field(type="password" label="Password", hint="The length must be more than 8 characters", v-model="password" required)
+          v-layout(row justify-space-between)
+            small *indicates required field
+            v-btn(color="primary" :loading="loading"  @click.prevent="sendRegister" :disabled="loading") Sign up
+          v-alert(error dismissible transition="scale-transition" v-model="alert") {{error}}
+        v-layout(row justify-space-between)
+          v-btn(color="primary" small flat :to="'/'") Have you an account?
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      loading: false,
+      alert: false,
+      error: ''
+    }
+  },
+  methods: {
+    async sendRegister() {
+      this.loading = true
+
+      try {
+        let userConfirmed = await this.$store.dispatch('auth/REGISTER', ({ username: this.email.trim(), password: this.password.trim() }))
+        !userConfirmed && this.$router.push({ name: 'verification' })
+      } catch (error) {
+        this.error = error.message
+        this.alert = true
+      }
+      this.loading = false
+    }
+  }
+}
+</script>
---
+++ pages/register.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  register
+</template>
+
+<script>
+import Register from '~/components/Register.vue'
+
+export default {
+  components: {
+    Register
+  }
+}
+</script>
