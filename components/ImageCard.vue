fa14b32154f6e6d2db57d0817207ef7e0493f2f9
Manuel Navarro Pérez <manuelnavarro1987@gmail.com>
2017-10-24 23:14:36 +0200

    New image card component

---
+++ components/ImageCard.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  v-card
+    v-card-media(:src="image.url" :contain="true" height="200px")
+    v-card-title(primary-title)
+      div
+        .headline {{image.name}}
+        span.grey--text {{dateFormat(image.lastModified)}}
+    v-card-actions
+      v-spacer
+      v-btn(icon)
+        v-icon(color="teal darken-2") share
+      v-btn(icon)
+        v-icon(color="deep-orange darken-2") delete
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+  props: [
+    'image'
+  ],
+  methods: {
+    dateFormat(input) {
+      return moment(input).format('L')
+    }
+  }
+}
+</script>
