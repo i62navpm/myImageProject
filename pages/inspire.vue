84e4741d7169c54bf600cd98d0a913f4d6d05379
Manuel Navarro Pérez <manuelnavarro1987@gmail.com>
2017-10-24 23:16:24 +0200

    Change in layout and pages

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,37 +1,25 @@
 <template lang="pug">
-v-container(fluid grid-list-md)
-  v-layout(row wrap)
-    v-flex.xs6(v-for="image in images" :key="image")
-      v-card
-        v-card-media(:src="image" :contain="true" height="200px")
-          //- v-container(fill-height fluid)
-          //-   v-layout(fill-height)
-          //-     v-flex(xs12 align-end flexbox)
-          //-       span.headline(v-text="'Hola'")
-        v-card-title(primary-title='')
-          div
-            .headline Top western road trips
-            span.grey--text 1,000 miles of wonder
-
-        v-card-actions
-          v-spacer
-          v-btn(icon='')
-            v-icon favorite
-          v-btn(icon='')
-            v-icon bookmark
-          v-btn(icon='')
-            v-icon share
+  v-container(fluid grid-list-md)
+    v-layout(row wrap)
+      v-flex.md4.sm6.xs12(v-for="image in images" :key="image.url")
+        image-card(:image="image")
+          
 </template>
 
 <script>
+import ImageCard from '~/components/ImageCard.vue'
+
 export default {
   layout: 'home',
+  components: {
+    ImageCard
+  },
   data () {
     return {
       images: []
     }
   },
-  async beforeCreate() {
+  async mounted() {
     this.images = await this.$store.dispatch('images/GET_IMAGES', {email: this.$store.state.auth.authEmail})
   }
 }
