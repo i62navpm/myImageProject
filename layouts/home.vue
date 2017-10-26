81163bbac1a686d8052803b41b3d819c70d1d572
Manuel Navarro Perez <manuelnavarro1987@gmail.com>
2017-10-26 23:48:54 +0200

    Remove navigation drawer

--- layouts/home.vue
+++ layouts/home.vue
@@ -1,18 +1,6 @@
 <template lang="pug">
   v-app
-    v-navigation-drawer(persistent='', :mini-variant='miniVariant', :clipped='clipped', v-model='drawer', app='')
-      v-list
-        v-list-tile(router='', :to='item.to', :key='i', v-for='(item, i) in items', exact='')
-          v-list-tile-action
-            v-icon(v-html='item.icon')
-          v-list-tile-content
-            v-list-tile-title(v-text='item.title')
     v-toolbar(dark color="primary" fixed='', app='', :clipped-left='clipped' extended)
-      v-toolbar-side-icon(@click='drawer = !drawer')
-      v-btn(icon='', @click.stop='miniVariant = !miniVariant')
-        v-icon(v-html="miniVariant ? 'chevron_right' : 'chevron_left'")
-      v-btn(icon='', @click.stop='clipped = !clipped')
-        v-icon web
       v-toolbar-title(v-text='title')
       v-spacer
       v-tooltip(left)
@@ -36,13 +24,6 @@ export default {
   },
   data () {
     return {
-      clipped: false,
-      drawer: false,
-      items: [
-        { icon: 'apps', title: 'Welcome', to: '/' },
-        { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
-      ],
-      miniVariant: false,
       title: 'My image project'
     }
   }
